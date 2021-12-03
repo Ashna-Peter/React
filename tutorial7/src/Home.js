f37548aee01bb34581e47c8d4160f4fb72938ef0
ashna <ashnapeter01@gmail.com>
2021-12-03 18:16:46 +0530

    feat: added click event

--- tutorial7/src/Home.js
+++ tutorial7/src/Home.js
@@ -1,7 +1,15 @@
 const Home = () => {
+    const handelClick = (e) =>{
+        console.log('hello ,ninja', e);
+    }
+    const handelClickAgain = (name, e) =>{
+        console.log('hello' + name , e.target);
+    }
     return ( 
         <div className="home">
             <h2>Homepage</h2>
+            <button onClick = {handelClick}>Click me</button>
+            <button onClick={(e) => handelClickAgain('mario' , e)}>Click me again</button>
         </div>
      );
 }
