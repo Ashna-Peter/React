38b2db11ca4e482c22607edd78ee55a2e2662257
ashna <ashnapeter01@gmail.com>
2021-12-08 11:47:40 +0530

    Exact Match Roues
    usee exact path to match the exact routes

--- tutorial22/src/App.js
+++ tutorial22/src/App.js
@@ -1,21 +1,24 @@
 import Navbar from './Navbar';
 import Home from './Home';
-import {BrowserRouter as Router ,Route, Switch} from 'react-router-dom';
-
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Create from './create';
 function App() {
- 
+
   return (
     <Router>
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        <Switch>
-          <Route path="/"> 
-            <Home />
-          </Route>
-        </Switch>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Switch>
+          <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/create">
+              <Create />
+            </Route>
+          </Switch>
+        </div>
       </div>
-    </div>
     </Router>
   );
 }
