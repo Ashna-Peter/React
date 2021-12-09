8e9fc2be36dcdac9a5f23d5df334ab8e300dad1d
ashna <ashnapeter01@gmail.com>
2021-12-09 11:00:42 +0530

    Submit Event

--- tutorial28/src/create.js
+++ tutorial28/src/create.js
@@ -2,11 +2,17 @@ import {useState} from "react";
 const Create = () => {
     const [title, setTitle] = useState();
     const [body, setBody] = useState();
-    const [author,setAuthor] = useState('mario'); 
+    const [author,setAuthor] = useState('mario');
+    const handleSubmit =(e) => {
+        e.preventDefault();
+        const blog = {title,body,author};
+
+        console.log(blog);
+     }
     return (
         <div className="create">
             <h2>Add a New Blog</h2>
-            <form >
+            <form onSubmit={handleSubmit}>
                 <label >Blog title:</label>
                 <input
                     type="text"
@@ -28,9 +34,9 @@ const Create = () => {
                     <option value="yoshi">yoshi</option>
                 </select>
                  <button>Add Blog</button>
-                 <p>{title}</p>
+                 {/* <p>{title}</p>
                  <p>{body}</p>
-                 <p>{author}</p>
+                 <p>{author}</p> */}
             </form>
         </div>
     );
